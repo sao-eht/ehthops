ae2502b11a9233f05002b616b6a7f1252f19061b
Iniyan Natarajan <iniyan.natarajan@cfa.harvard.edu>
2024-05-08 15:47:52 -0400

    Update postprocessing driver script

--- ehthops/scripts/driver_postproc.sh
+++ ehthops/scripts/driver_postproc.sh
@@ -5,20 +5,25 @@ source /n/holylfs05/LABS/bhi/Lab/doeleman_lab/inatarajan/software/installed/hops
 
 stages=("7.+apriori") # , "8.+polcal", "9.+netcal")
 
+# working directory name
+workdir=$(pwd)
+
 for stage in ${stages[@]}
 do
     echo "Starting stage $stage..."
     cd $stage
-    pwd
+    echo "cd into $(pwd)"
 
     source bin/0.launch -f 230 -t eht -y 2021
-    source bin/1.version
 
     if [ $stage == "7.+apriori" ]
     then
-        source bin/2.apriori
+        source bin/1.apriori
+	source bin/2.import
+        python bin/3.average
     fi
 
     cd ..
+    echo "cd up to $(pwd)"
     echo "Finished stage $stage..."
 done
