ef3bced676f5589cbcd88d51331f6e3ad5163ef8
Iniyan Natarajan <iniyan.natarajan@cfa.harvard.edu>
2024-10-17 15:43:21 -0400

    Update sample driver scripts

--- ehthops/scripts/driver_cannon.sh
+++ ehthops/scripts/driver_cannon.sh
@@ -1,8 +1,8 @@
 # To be run from within 'hops-bx' directories
 # set up envs
 source $HOME/.bashrc
-mamba activate ehthops310
-source /n/holylfs05/LABS/bhi/Lab/doeleman_lab/inatarajan/software/installed/hops-3.24/bin/hops.bash
+mamba activate ehthops310 # activate the mamba environment with the necessary packages installed
+source /path/to/hops.bash
 
 # list of stages to run
 stages=("0.bootstrap" "1.+flags+wins" "2.+pcal" "3.+adhoc" "4.+delays" "5.+close" "6.uvfits")
@@ -21,7 +21,7 @@ do
     # run fourfit for stages 0-5
     if [ $stage != "6.uvfits" ]
     then
-        SET_SRCDIR=/path/to/data/archive && SET_CORRDAT="Rev1-Cal:Rev1-Sci" && source bin/0.launch -t /path/to/metadata -y 2021 -m -d 4 -p "e21f.*-$band-.*.hops/"
+        SET_SRCDIR=/path/to/data/archive && SET_CORRDAT="Rev1-Cal:Rev1-Sci" && SET_METADIR=/path/to/metadata && source bin/0.launch -y 2021 -m -d 4 -p "e21f.*-$band-.*.hops/"
         source bin/1.version
         source bin/2.link
         source bin/3.fourfit
@@ -51,10 +51,10 @@ do
         source bin/7.close
     fi
 
-    # run stage 6 after the 5 fringe-fitting stages
+    # run stage 6 after the 5 fringe-fitting stages; SRCDIR is now 5.+close/data
     if [ $stage == "6.uvfits" ]
     then
-	    SET_EHTIMPATH="/n/holylfs05/LABS/bhi/Lab/doeleman_lab/inatarajan/software/src/eht-imaging" && SET_SRCDIR=$workdir/5.+close/data && SET_CORRDAT="Rev1-Cal:Rev1-Sci" && source bin/0.launch -t /path/to/metadata -y 2021 -m -d 4 -p "e21f.*-$band-.*.hops/"
+	    SET_EHTIMPATH="/path/to/eht-imaging" && SET_SRCDIR=$workdir/5.+close/data && SET_METADIR=/path/to/metadata && source bin/0.launch -c EHT2021
         source bin/1.convert
         source bin/2.import
         python bin/3.average
--- ehthops/scripts/driver_postproc.sh
+++ ehthops/scripts/driver_postproc.sh
@@ -1,12 +1,14 @@
 # set up envs
 source $HOME/.bashrc
 mamba activate ehthops310
-source /n/holylfs05/LABS/bhi/Lab/doeleman_lab/inatarajan/software/installed/hops-3.24/bin/hops.bash
+source /path/to/installed/hops/bin/hops.bash
 
 stages=("7.+apriori") # , "8.+polcal", "9.+netcal")
 
 # working directory name
 workdir=$(pwd)
+# extract band from working directory
+band=$(echo $workdir | sed 's|.*/hops-\(..\)/\?.*$|\1|')
 
 for stage in ${stages[@]}
 do
@@ -14,13 +16,12 @@ do
     cd $stage
     echo "cd into $(pwd)"
 
-    source bin/0.launch -t /path/to/metadata -y 2021 -m -d 4 -p "e21f.*-$band-.*.hops/"
-
     if [ $stage == "7.+apriori" ]
     then
+	    SET_EHTIMPATH="/path/to/eht-imaging" && SET_SRCDIR=$workdir/6.uvfits && SET_METADIR=/path/to/metadata && source bin/0.launch -c EHT2021
         source bin/1.apriori
-	    source bin/2.import
-        python bin/3.average
+        #source bin/2.import
+        #python bin/3.average
     fi
 
     cd ..
